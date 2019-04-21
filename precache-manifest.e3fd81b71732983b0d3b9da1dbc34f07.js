24035fe7e614c1d4cdfc49e9cb7d45083888c3dc
mohdmaroof <maroofs.md@gmail.com>
2019-04-21 12:10:52 +0530

    Updates

---
+++ precache-manifest.e3fd81b71732983b0d3b9da1dbc34f07.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d20161109ba08d3ef3a965a122015e5",
+    "url": "/germs/static/media/fine_modern_art.1d201611.svg"
+  },
+  {
+    "revision": "c19e904d2772821de9db",
+    "url": "/germs/static/css/main.a043f011.chunk.css"
+  },
+  {
+    "revision": "4141be92524bf9f80ee017e81283b9d7",
+    "url": "/germs/static/media/call_action.4141be92.PNG"
+  },
+  {
+    "revision": "23a0bf79f0fd30d193d4",
+    "url": "/germs/static/js/1.23a0bf79.chunk.js"
+  },
+  {
+    "revision": "1498bf1353978730953a",
+    "url": "/germs/static/js/runtime~main.1498bf13.js"
+  },
+  {
+    "revision": "b3c333f90db09ab4b692b3006d2c7f06",
+    "url": "/germs/static/media/Linkedin_icon.b3c333f9.svg"
+  },
+  {
+    "revision": "dee588fc62cfe0eb99732f2ff038d5b9",
+    "url": "/germs/static/media/hire_us_visual.dee588fc.svg"
+  },
+  {
+    "revision": "57fd835ad2cb24ee1f1c99746c5af841",
+    "url": "/germs/static/media/Instagrame_icon.57fd835a.svg"
+  },
+  {
+    "revision": "f1a2170fa1b40af0c3b068727abd257e",
+    "url": "/germs/static/media/Pintrest_icon.f1a2170f.svg"
+  },
+  {
+    "revision": "46d31ede4147f6800c8ea6057d1d858d",
+    "url": "/germs/static/media/Behance_icon.46d31ede.svg"
+  },
+  {
+    "revision": "982a26aee2cc28056c943b8666d69c3c",
+    "url": "/germs/static/media/Dribbble_icon.982a26ae.svg"
+  },
+  {
+    "revision": "c0e4650eaf1cb8cc000e4a639ab0fa30",
+    "url": "/germs/static/media/facebook_icon.c0e4650e.svg"
+  },
+  {
+    "revision": "fca4d0d4901d8091054906020608a363",
+    "url": "/germs/static/media/scroll_up_icon.fca4d0d4.svg"
+  },
+  {
+    "revision": "759c9f15cb9225038a8f91af27f493c3",
+    "url": "/germs/static/media/test2.759c9f15.png"
+  },
+  {
+    "revision": "f5bf1a24038c083d20e2089810d67b57",
+    "url": "/germs/static/media/test1.f5bf1a24.jpg"
+  },
+  {
+    "revision": "16a99e47f9990333886725d59626951c",
+    "url": "/germs/static/media/share_icon.16a99e47.svg"
+  },
+  {
+    "revision": "bedb8dce6f5febb74d4625323be0fab4",
+    "url": "/germs/static/media/back_arrow.bedb8dce.svg"
+  },
+  {
+    "revision": "0eef3a72822c7713c35e69a92454db23",
+    "url": "/germs/static/media/wallpaper.0eef3a72.PNG"
+  },
+  {
+    "revision": "c940a36e8b7b390a31883378cd15bc70",
+    "url": "/germs/static/media/texture.c940a36e.PNG"
+  },
+  {
+    "revision": "a2b5cf21fc172177931f4cd5d267eb3c",
+    "url": "/germs/static/media/home_page_visual.a2b5cf21.svg"
+  },
+  {
+    "revision": "1822410498e242f5a0e7e9b4b18e1290",
+    "url": "/germs/static/media/Germs_Logo.18224104.svg"
+  },
+  {
+    "revision": "dfbbca57c1b751d2c9c0daa9542facc6",
+    "url": "/germs/static/media/scroll_down_icon.dfbbca57.svg"
+  },
+  {
+    "revision": "6c37db73271653208b0d09ce7d53f993",
+    "url": "/germs/static/media/ui_web_mobile.6c37db73.svg"
+  },
+  {
+    "revision": "ce3df0b345f11b9cdbb00b462282d11e",
+    "url": "/germs/static/media/branding_visual_identity.ce3df0b3.svg"
+  },
+  {
+    "revision": "0c104ac28c4590f94b52d1c8e7d33a07",
+    "url": "/germs/static/media/digital_marketing.0c104ac2.svg"
+  },
+  {
+    "revision": "cf9c790dc4c6191ac10b54bb785df018",
+    "url": "/germs/static/media/product_photography.cf9c790d.svg"
+  },
+  {
+    "revision": "c19e904d2772821de9db",
+    "url": "/germs/static/js/main.c19e904d.chunk.js"
+  },
+  {
+    "revision": "3eb5944d0ad70014746a2a05773f88bc",
+    "url": "/germs/static/media/1_Client_logo.3eb5944d.svg"
+  },
+  {
+    "revision": "86d816b949ccc788e2007053e3dade7d",
+    "url": "/germs/static/media/2_Client_logo.86d816b9.svg"
+  },
+  {
+    "revision": "e2672059ac130646d28047bf9d3bc848",
+    "url": "/germs/static/media/3_Client_logo.e2672059.svg"
+  },
+  {
+    "revision": "5dc31dd5866ae08e5beba901f29c5e48",
+    "url": "/germs/static/media/4_Client_logo.5dc31dd5.svg"
+  },
+  {
+    "revision": "26707a635882a99e2e9069a11bbf7de3",
+    "url": "/germs/static/media/5_Client_logo.26707a63.svg"
+  },
+  {
+    "revision": "4b768cb53a2269d3b30ce1b7dbc33daf",
+    "url": "/germs/static/media/6_Client_logo.4b768cb5.svg"
+  },
+  {
+    "revision": "fcd822bd57c400fa258cc5c4a04336fe",
+    "url": "/germs/static/media/7_Client_logo.fcd822bd.svg"
+  },
+  {
+    "revision": "ea338caf9ab5020fa84d16d755105efc",
+    "url": "/germs/static/media/8_Client_logo.ea338caf.svg"
+  },
+  {
+    "revision": "54055acbfeb94549bac1ce001ac25ad3",
+    "url": "/germs/static/media/Attached_icon.54055acb.svg"
+  },
+  {
+    "revision": "f3558ce96f378bd9f8f09aa5ff676789",
+    "url": "/germs/static/media/nature.f3558ce9.PNG"
+  },
+  {
+    "revision": "02367652f9146c03cbbb08f9cb30fe7c",
+    "url": "/germs/static/media/people.02367652.PNG"
+  },
+  {
+    "revision": "bdf4f0426a1c0655818fda5f3b181da6",
+    "url": "/germs/static/media/architecture.bdf4f042.PNG"
+  },
+  {
+    "revision": "86845232b1194c3458721e4dd8ebd28c",
+    "url": "/germs/static/media/abstract.86845232.PNG"
+  },
+  {
+    "revision": "3e60bbb4a48a531dfbfc3c246d039da1",
+    "url": "/germs/static/media/saifee_khan.3e60bbb4.png"
+  },
+  {
+    "revision": "a69853820b912d0363506f7011a098bd",
+    "url": "/germs/static/media/sam_khan.a6985382.png"
+  },
+  {
+    "revision": "bc8d983f140e10bef3ccb7d836071a07",
+    "url": "/germs/static/media/shahid_raza.bc8d983f.png"
+  },
+  {
+    "revision": "03a4e242465a5ca2aa10c4a7f89c9b3d",
+    "url": "/germs/static/media/prasham_gupta.03a4e242.png"
+  },
+  {
+    "revision": "991e8607b00ce5fffc70ca13d84dc5de",
+    "url": "/germs/static/media/shashank_arya.991e8607.png"
+  },
+  {
+    "revision": "50aad290b8f6c58b524984b453f2bdbe",
+    "url": "/germs/static/media/jahangir_alam.50aad290.png"
+  },
+  {
+    "revision": "75e9ad308cbe997e234baac178037d5a",
+    "url": "/germs/static/media/shahbaz_khan.75e9ad30.png"
+  },
+  {
+    "revision": "9a3cef9afd00e13fcc5e3e1883da27b1",
+    "url": "/germs/static/media/taki_khan.9a3cef9a.png"
+  },
+  {
+    "revision": "15907af9b3cdb38fb63f51083d21aef5",
+    "url": "/germs/static/media/apple_inc.15907af9.PNG"
+  },
+  {
+    "revision": "0e0af0478fb70a1027c760028a0a706a",
+    "url": "/germs/static/media/mobile_action.0e0af047.PNG"
+  },
+  {
+    "revision": "23a0bf79f0fd30d193d4",
+    "url": "/germs/static/css/1.8a67a5cc.chunk.css"
+  },
+  {
+    "revision": "15572e36a6d6e78057502402273aec48",
+    "url": "/germs/index.html"
+  }
+];
